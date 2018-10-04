3b218c414daab08391a2a86fc76ccf1e8b0ad6d2
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-03 23:35:01 -0600

    Updated emploee vue assets

--- Resources/components/EmployeeData.vue
+++ Resources/components/EmployeeData.vue
@@ -1,32 +1,25 @@
 <template>
-    <div id="employee-data">
 
-        <form action="" method="post">
-            <form-group :type="'text'"></form-group>
-        </form>
-    </div>
 </template>
 
 <script>
 export default {
-    name: "employee-data",
-    props: ["model", "employee"],
-    data(){
-        return{
-            first_name: ""
-        }
-    },
-    watch: {
-        first_name(first_name){
-            if(!first_name) {
-                return this.employee.employee.first_name || "";
-            }
-        }
+  name: "employee-data",
+  props: ["model", "employee"],
+  data() {
+    return {
+      first_name: ""
+    };
+  },
+  watch: {
+    first_name(first_name) {
+      if (!first_name) {
+        return this.employee.employee.first_name || "";
+      }
     }
-
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
