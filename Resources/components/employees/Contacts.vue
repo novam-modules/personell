bc3e421804f923a5549bdefae0a70fa7f7f58adb
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-07 00:36:41 -0600

    Added new employees vue components

---
+++ Resources/components/employees/Contacts.vue
@@ -0,0 +1,42 @@
+<template>
+    <table width="100%" class="user-table">
+        <tbody>
+            <tr>
+                <td width="30px"><i class="icon-email"></i></td>
+                <td width="33%"><strong>Username:</strong></td>
+                <td> {{ employee.email }} </td>
+            </tr>
+            <tr>
+                <td><i class="icon-telephone"></i></td>
+                <td><strong>Office Phone:</strong></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><i class="icon-telephone"></i></td>
+                <td><strong>Cell Phone:</strong></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><i class="icon-telephone"></i></td>
+                <td><strong>Fax:</strong></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "employee-contacts",
+        props: ["employee", "empno"],
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Resources/components/employees/Profile.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <span>{{ employee.email }}</span>
+        <br>
+        <span>
+            <br>
+            , UT
+            <br>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "employee-profile",
+        props: ["employee", "empno"],
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
